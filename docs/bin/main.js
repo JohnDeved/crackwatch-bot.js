9b63405f9e20d2b1044ed9c5f869ae8a7a1427b3
DevSec <arma.devsec@gmail.com>
2017-10-27 23:32:04 +0200

    + log

--- docs/bin/main.js
+++ docs/bin/main.js
@@ -1,6 +1,7 @@
 if (window.location.hash !== '') {
   var hash = window.location.hash.substr(1)
   $.get('https://api.xrel.to/v2/release/info.json?dirname=' + hash, function (data) {
+    console.log(data)
     if (data.error) {
       console.log('nfo not available yet!')
     } else {
