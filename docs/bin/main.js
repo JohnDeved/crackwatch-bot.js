c7e977a4c0a84adc5f3dc7e98d4d2bf393642430
DevSec <arma.devsec@gmail.com>
2017-10-27 23:23:10 +0200

    hash

--- docs/bin/main.js
+++ docs/bin/main.js
@@ -1,7 +1,13 @@
 if (window.location.hash !== '') {
-  $.get('ajax/test.html', function (data) {
-    $('.result').html(data)
-    alert('Load was performed.')
+  var hash = window.location.hash.substr(1)
+  $.get('https://api.xrel.to/v2/release/info.json?dirname=' + hash, function (data) {
+    if (data.error) {
+      console.log('nfo not available yet!')
+    } else {
+      $.get(data.link_href, function (data) {
+        console.log(data)
+      })
+    }
   })
 } else {
   window.history.back()
