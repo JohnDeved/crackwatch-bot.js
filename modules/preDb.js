914bcf51f18540f7e0a6c40d1473adda2f04eb40
DevSec <arma.devsec@gmail.com>
2017-10-28 18:59:30 +0200

    ~ move xrel

--- modules/preDb.js
+++ modules/preDb.js
@@ -6,7 +6,7 @@ const PreDb = class {
   constructor () {
     this.check = callback => {
       request.get({url: 'http://predb.me/?cats=games-pc', headers: CONFIG.headers}, (err, response, body) => {
-        err && console.error(err)
+        if (err) { return console.error(err) }
         response && console.info('predb.me statusCode:'.grey, response.statusCode, response.statusMessage.grey)
 
         const $ = cheerio.load(body)
@@ -36,7 +36,7 @@ const PreDb = class {
 
     this.info = (id, callback) => {
       request.get({url: `http://predb.me/?post=${id}&jsload=1`, headers: CONFIG.headers}, (err, response, body) => {
-        err && console.error(err)
+        if (err) { return console.error(err) }
         response && console.info('jsload statusCode:'.grey, response.statusCode, response.statusMessage.grey)
 
         const $ = cheerio.load(body)
@@ -46,12 +46,7 @@ const PreDb = class {
         info.size = $('.pb-c:contains(Size)').next().text()
         info.genres = $('.pb-c:contains(Genres)').next().text()
         info.tags = $('.pb-c:contains(Tags)').next().text()
-
-        request.get(`https://api.xrel.to/v2/release/info.json?dirname=${info.Rlsname}`, (err, response, body) => {
-          info.xrel = JSON.parse(body)
-          info.xrel.error && console.error('xrel error:'.red, info.xrel.error_description, info.Rlsname.grey)
-          callback(info)
-        })
+        callback(info)
       })
     }
   }
