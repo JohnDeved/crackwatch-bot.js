cd2850f37dc1f49d00cb02d71c2b3aafbd0f43b8
DevSec <arma.devsec@gmail.com>
2017-10-30 05:02:27 +0100

    - xrel

--- modules/xrel.js
+++
@@ -1,4 +0,0 @@
-// request.get(`https://api.xrel.to/v2/release/info.json?dirname=${info.Rlsname}`, (err, response, body) => {
-//   info.xrel = JSON.parse(body)
-//   info.xrel.error && console.error('xrel error:'.red, info.xrel.error_description, info.Rlsname.grey)
-// })
